0098cc2f89e47b47879c53c08b8e4b8a644b3588
abdulmalik alghazi <malka2974@gmail.com>
2021-06-14 10:04:09 +0400

    created enemy script and controller

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,14 +4,46 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    public int damage = 10;
     public GameObject hitEffect;
+    public bool isEnemyBullet = false;
+
+    public float speed = 10f;
+
+
+    private Rigidbody2D rb;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = transform.up * speed;
+
+        if (isEnemyBullet)
+        {
+            rb.AddTorque(5f, ForceMode2D.Impulse);
+        }
+
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
-        Destroy(effect, 4f);
-        Destroy(gameObject);
-/**/
+
+        if (isEnemyBullet)
+        {
+            if (collision.gameObject.tag != "Enemy")
+            {
+                GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+                Destroy(effect, 4f);
+                Destroy(gameObject);
+            }
+        }
+        else 
+        { 
+            if (collision.gameObject.tag != "Player")
+            {
+                GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+                Destroy(effect, 4f);
+                Destroy(gameObject);
+            }
+        }
+
     }
 }
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public int reward = 10;
+    public int damage = 10;
+    public int health = 10;
+
+    public GameObject deathEffect;
+
+    Player player;
+    Rigidbody2D rb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+
+    }
+
+    void Die()
+    {
+        Destroy(this.gameObject);
+        GameObject effect = Instantiate(deathEffect, transform.position, Quaternion.identity);
+        Destroy(effect, 4f);
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag =="Player")
+        {
+            Die();
+
+        }
+
+        else if (collision.gameObject.tag == "Bullet")
+        {
+
+            Die();
+        }
+    }
+}
