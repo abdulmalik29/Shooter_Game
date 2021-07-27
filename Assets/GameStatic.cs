e7645dea1b29030f332a0109966ea2a29743c92d
abdulmalik alghazi <malka2974@gmail.com>
2021-07-27 22:12:48 +0400

    added a leaderboard

---
+++ Assets/GameStatic.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStatic
+{
+    public static bool isLogedin = false;
+    public static string userName = "";
+}
---
+++ Assets/Login.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using PlayFab;
+using PlayFab.ClientModels;
+
+public class Login : MonoBehaviour
+{
+    public Text StatusText;
+
+    bool isLoading = false;
+    bool shouldCreateANewAcount = false;
+
+    string customId = "";
+    string CUSTOM_KEY_ID = "CUSTOM_KEY_ID";
+
+    void Awake()
+    {
+        Auth();
+    }
+
+
+    public void Auth()
+    {
+        
+    }
+
+    public void UseNewUser()
+    {
+        GameStatic.isLogedin = false;
+        PlayerPrefs.DeleteKey(CUSTOM_KEY_ID);
+        Auth();
+    }
+
+
+}
